2c7245e52d95520ab04ce1988dbbe2e377595633
Allen Snook <allen@allendav.com>
2020-01-28 09:43:59 -0800

    Initial commit

---
+++ woocommerce-payments-dev-tools.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WooCommerce Payments Developer Tools
+Plugin URI: https://woocommerce.com
+Description: Makes testing WooCommerce Payments a bit easier.
+Author: allendav
+Version: 0.1
+Author URI: https://allendav.com
+*/
+
+define( 'WCPAY_DEV_MODE', true );
