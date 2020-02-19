57f65c0f788dfb723021e96fad108b59296daffa
Allen Snook <allendav@automattic.com>
2020-02-19 12:20:30 -0800

    Update woocommerce-payments-dev-tools.php

--- woocommerce-payments-dev-tools.php
+++ woocommerce-payments-dev-tools.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: WooCommerce Payments Developer Tools
+Plugin Name: WooCommerce Payments Dev Mode
 Plugin URI: https://woocommerce.com
-Description: Makes testing WooCommerce Payments a bit easier.
+Description: Sets WCPAY_DEV_MODE to true
 Author: allendav
 Version: 0.1
 Author URI: https://allendav.com
