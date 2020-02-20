b880fb0f98c47e374e77b949e8883081ecd1c291
Allen Snook <allen@allendav.com>
2020-02-20 14:54:58 -0800

    Add notice when this plugin is active

--- woocommerce-payments-dev-mode.php
+++ woocommerce-payments-dev-mode.php
@@ -9,3 +9,10 @@ Author URI: https://allendav.com
 */
 
 define( 'WCPAY_DEV_MODE', true );
+
+function wcpay_dev_mode_notice() {
+    $class = 'notice notice-error';
+    $message = __( 'WARNING: WCPAY_DEV_MODE enabled' );
+    echo '<div class="notice notice-error"><p>WARNING: WCPAY_DEV_MODE enabled</p></div>';
+}
+add_action( 'admin_notices', 'wcpay_dev_mode_notice' );
