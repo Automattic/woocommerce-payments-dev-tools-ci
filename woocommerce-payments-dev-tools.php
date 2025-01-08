768f3829c430eecfa1b87800227e9b558a86cbdb
Guilherme Pressutto <gpressutto5@gmail.com>
2025-01-08 11:18:40 -0300

    Revert "Remove obsolete Tokenized Cart ECE flag" (#133)

--- woocommerce-payments-dev-tools.php
+++ woocommerce-payments-dev-tools.php
@@ -28,6 +28,7 @@ class WC_Payments_Dev_Tools {
 	const BILLING_CLOCKS_OPTION = 'wcpaydev_wcpay_billing_clock';
 	const BILLING_CLOCK_SECRET_KEY_OPTION = 'wcpay_billing_clock_secret';
 	const SUBSCRIPTIONS = '_wcpay_feature_subscriptions';
+	const TOKENIZED_CART_ECE_FLAG_NAME = '_wcpay_feature_tokenized_cart_ece';
 	const CAPITAL = '_wcpay_feature_capital';
 	const DOCUMENTS = '_wcpay_feature_documents';
 	const WOOPAY_OVERRIDE_PLATFORM_CHECKOUT_ELIGIBLE = 'override_woopay_eligible';
@@ -704,6 +705,7 @@ class WC_Payments_Dev_Tools {
 		self::save_option_from_checkbox( self::DOCUMENTS );
 
 		self::save_option_from_checkbox( self::V2_GATEWAY_FLAG_NAME );
+		self::save_option_from_checkbox( self::TOKENIZED_CART_ECE_FLAG_NAME );
 		self::save_option_from_checkbox( self::WOOPAY_OVERRIDE_PLATFORM_CHECKOUT_ELIGIBLE, true );
 		self::save_option( self::WOOPAY_OVERRIDE_PLATFORM_CHECKOUT_ELIGIBLE_VALUE );
 		self::save_option_from_checkbox( self::WOOPAY_OVERRIDE_PLATFORM_CHECKOUT_DEFAULT_OPT_IN, true );
@@ -1049,6 +1051,7 @@ class WC_Payments_Dev_Tools {
 					</div>
 
 					<?php self::render_checkbox( self::V2_GATEWAY_FLAG_NAME, 'Enable V2 gateway' ); ?>
+					<?php self::render_checkbox( self::TOKENIZED_CART_ECE_FLAG_NAME, 'Enable Cart-Token implementation for ECE' ); ?>
 					<?php self::render_checkbox( self::CAPITAL, 'Enable Stripe Capital' ); ?>
 					<?php self::render_checkbox( self::DOCUMENTS, 'Enable WCPay Documents section' ); ?>
 
@@ -1320,6 +1323,10 @@ class WC_Payments_Dev_Tools {
 
 		if ( get_option( self::DOCUMENTS, false ) ) {
 			$enabled_options[] = 'Account Documents section';
+		}
+
+		if ( get_option( self::TOKENIZED_CART_ECE_FLAG_NAME, false ) ) {
+			$enabled_options[] = 'Tokenized cart ECE';
 		}
 
 		if ( get_option( self::WOOPAY_OVERRIDE_PLATFORM_CHECKOUT_ELIGIBLE, '0' ) ) {
